34af02e29fc896c02eba2e223b23f301d9416f93
José Pereira <josep16527@agvv.edu.pt>
2023-12-01 17:03:41 +0000

    main

--- src/Backend/Utilizador.java
+++ src/Backend/Utilizador.java
@@ -1,10 +1,19 @@
 package Backend;
 
 public class Utilizador {
+    private String username;
     private String email;
     private String password;
 
     public Utilizador() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
---
+++ src/Frontend/Login.java
@@ -0,0 +1,10 @@
+package Frontend;
+
+import javax.swing.*;
+
+public class Login {
+    private JTextField tfPass;
+    private JTextField tfUser;
+    private JButton button1;
+    private JButton button2;
+}
